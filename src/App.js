894e95af0bd598bbd964b1a1cb529f04b1669004
Ismoil Hamidzoda <ismoil2888@gmail.com>
2024-10-26 12:58:39 +0500

    route update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Routes, Route, useNavigate  } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
-import { auth } from './firebase'; // Импорт Firebase auth
+import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css';
+import { auth } from './firebase';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import AuthDetails from './components/AuthDetails';
@@ -23,44 +23,43 @@ import TeacherLogin from './components/TeacherLogin';
 import TeacherProfile from './components/TeacherProfile';
 
 function App() {
-  const navigate = useNavigate();
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Состояние для отслеживания авторизации
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // состояние для загрузки
 
   useEffect(() => {
-    // Проверка авторизации пользователя при запуске приложения
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setIsAuthenticated(true); // Пользователь авторизован
-        navigate('/home'); // Перенаправляем на главную страницу
-      } else {
-        setIsAuthenticated(false); // Пользователь не авторизован
-        navigate('/'); // Перенаправляем на страницу входа
-      }
+      setIsAuthenticated(!!user);
+      setIsLoading(false); // завершаем загрузку после получения статуса авторизации
     });
 
-    return () => unsubscribe(); // Отписка при размонтировании
-  }, [navigate]);
+    return () => unsubscribe();
+  }, []);
+
+  if (isLoading) {
+    // Показываем загрузочный экран, пока идет проверка
+    return <div>Loading...</div>;
+  }
 
   return (
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/authdetails" element={<AuthDetails />} />
-          <Route path="/about" element={<PrivateRoute> <About /> </PrivateRoute>} />
-          <Route path="/home" element={<PrivateRoute>  <HomePage /> </PrivateRoute>} />
-          <Route path="/library" element={<PrivateRoute> <Library /> </PrivateRoute>} />
-          <Route path="/schedule" element={<PrivateRoute> <Schedule /> </PrivateRoute>} />
-          <Route path="/teachers" element={<PrivateRoute> <Teachers /> </PrivateRoute>} />
-          <Route path="/contacts" element={<PrivateRoute> <Contacts /> </PrivateRoute>} />
-          <Route path="/welcomepage" element={<WelcomePage />} />
-          <Route path="/library" element={<LibraryPage />} />
-          <Route path="/teachers" element={<TeachersPage />} />
-          <Route path="/teacher-login" element={<TeacherLogin />} />
-          <Route path="/teacher-profile/:id" element={<TeacherProfile />} />
-          <Route path="/admin" element={<AdminPrivateRoute> <AdminPanel /> </AdminPrivateRoute>} />
-          <Route path="/admin-login" element={<AdminLogin />} />
-          <Route path="*" element={<NotfoundPage />} />
-        </Routes>
+    <Routes>
+      <Route path="/" element={isAuthenticated ? <HomePage /> : <SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/authdetails" element={<AuthDetails />} />
+      <Route path="/about" element={<PrivateRoute> <About /> </PrivateRoute>} />
+      <Route path="/home" element={<PrivateRoute>  <HomePage /> </PrivateRoute>} />
+      <Route path="/library" element={<PrivateRoute> <Library /> </PrivateRoute>} />
+      <Route path="/schedule" element={<PrivateRoute> <Schedule /> </PrivateRoute>} />
+      <Route path="/teachers" element={<PrivateRoute> <Teachers /> </PrivateRoute>} />
+      <Route path="/contacts" element={<PrivateRoute> <Contacts /> </PrivateRoute>} />
+      <Route path="/welcomepage" element={<WelcomePage />} />
+      <Route path="/library" element={<LibraryPage />} />
+      <Route path="/teachers" element={<TeachersPage />} />
+      <Route path="/teacher-login" element={<TeacherLogin />} />
+      <Route path="/teacher-profile/:id" element={<TeacherProfile />} />
+      <Route path="/admin" element={<AdminPrivateRoute> <AdminPanel /> </AdminPrivateRoute>} />
+      <Route path="/admin-login" element={<AdminLogin />} />
+      <Route path="*" element={<NotfoundPage />} />
+    </Routes>
   );
 }
 
